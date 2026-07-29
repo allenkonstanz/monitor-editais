947eb0a4e614620ab712ba2ae77fae462b52c049
allenkonstanz <santiago.staviski@gmail.com>
2026-07-29 11:45:42 -0300

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@
 
 OUTPUT_HTML_FILE = "index.html"
 CHECK_INTERVAL_MINUTES = 10080
-MAX_EDITAL_AGE_DAYS = 90  # Oculta editais com mais de 30 dias automaticamente
+MAX_EDITAL_AGE_DAYS = 90  # Oculta editais com mais de 90 dias automaticamente
 
 PALAVRAS_IGNORADAS = [
     "mestrado",
